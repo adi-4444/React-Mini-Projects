c142eb9fd307f622faba1ecf1aa00277a10103b7
Adi <adinarayana4444@gmail.com>
2022-09-20 23:36:26 +0530

    React Mini Projects

---
+++ src/randomUserApi/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Random User Api</h1>
+      <h2>Name</h2>
+      <p>Email</p>
+    </div>
+  )
+}
+
+export default App
